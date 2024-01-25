55aa4b48a2435e0660e5ab85af11812a2edaab57
Jeremy-Harper <39338503+Jeremy-Harper@users.noreply.github.com>
2024-01-25 15:51:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,13 +14,8 @@ if __name__ == '__main__':
         exit(1)
 
     out_path = args.output_path or os.path.dirname(args.input_file)
-    command = f'nougat "{args.input_file}" -o "{out_path}" -m 0.1.0-base'
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, error = process.communicate()
-    if process.returncode != 0:
-        print(error.decode())
-        exit(1)
+    
 
-    mp3_gen = MP3Generator(os.path.join(out_path, os.path.basename(args.input_file).replace('.pdf', '.mmd')))
+    mp3_gen = MP3Generator(os.path.join(out_path, os.path.basename(args.input_file)))
     mp3_files = mp3_gen.generate_mp3_files()
     merge_mp3_files(out_path, mp3_files)
